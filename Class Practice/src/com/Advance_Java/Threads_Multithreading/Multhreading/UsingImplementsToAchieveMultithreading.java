b80b2e1405deea382fd5825e51b2a16b973bd7de
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-06-07 00:25:40 +0530

    Priority of Threads

--- Class Practice/src/com/Advance_Java/Threads_Multithreading/Multhreading/UsingImplementsToAchieveMultithreading.java
+++ Class Practice/src/com/Advance_Java/Threads_Multithreading/Multhreading/UsingImplementsToAchieveMultithreading.java
@@ -1,20 +1,11 @@
 package com.Advance_Java.Threads_Multithreading.Multhreading;
 
 import java.util.Scanner;
-
 /*
-Q1. What is Process?
-    It's a program which is present on the RAM which is ready for the execution or which is executing already.
-
-Q2. What is Thread?
-    Thread is a independent instructions getting executed independently within a process.
-
-if you want to achieve multithreading - you need to keep the independent method inside run() method which is present in Thread Class
-we can achieve multithreading in 2 ways -> 1. By extends class (Thread Class)
-                                        -> 2. By implements interface (Runnable interface)
+Q. Which one is more effective/prefered More for multi-threading, is it implementing Runnable interface or is it extending Thread class?
+->
  */
-
-class Demo1 extends Thread {
+class Demo11 implements Runnable{
     @Override
     public void run() {
         Scanner sc = new Scanner(System.in);
@@ -28,7 +19,7 @@ class Demo1 extends Thread {
     }
 
 }
-class Demo2  extends Thread {
+class Demo12 implements Runnable {
     @Override
     public void run() {
         System.out.println("Printing characters started");
@@ -45,11 +36,11 @@ class Demo2  extends Thread {
     }
 
 }
-class Demo3 extends Thread {
+class Demo13 implements Runnable {
     @Override
     public void run() {
         System.out.println("Printing numbers started");
-        for(int i = 1; i <= 10 ; i++) {
+        for(int i = 1; i <= 5 ; i++) {
             System.out.println(i);
             try {
                 Thread.sleep(3000);
@@ -62,27 +53,19 @@ class Demo3 extends Thread {
     }
 
 }
-public class UnderstandingMultithreading {
+public class UsingImplementsToAchieveMultithreading {
     public static void main(String[] args) {
-        Demo1 d1 = new Demo1();
-        Demo2 d2 = new Demo2();
-        Demo3 d3 = new Demo3();
-//        d1.run();  // it will not work like to call run() method you need call start() method
-//        d2.run();
-//        d3.run();
-        d1.start();
-        d2.start();
-        d3.start();
+        Demo11 demo11 = new Demo11();
+        Demo12 demo12 = new Demo12();
+        Demo13 demo13 = new Demo13();
+
+        Thread thread1 = new Thread(demo11);
+        Thread thread2 = new Thread(demo12);
+        Thread thread3 = new Thread(demo13);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
