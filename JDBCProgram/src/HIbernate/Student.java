75c78db6497199e129ddf3a3fcff5356bcf961dc
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-06-29 14:19:20 +0530

    Added doc related to Hibernate

--- JDBCProgram/src/HIbernate/Student.java
+++ JDBCProgram/src/HIbernate/Student.java
@@ -1,4 +1,54 @@
 package HIbernate;
 
 public class Student {
+    int id;
+    String name;
+    int age;
+    String gender;
+    int mark;
+
+    public Student () {
+
+    }
+    public Student (int id, String name, int age, String gender, int mark) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.mark = mark;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getMark() {
+        return mark;
+    }
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+    @Override
+    public String toString() {
+        return "name=" + name + ", age=" + age + ", gender=" + gender + ", mark=" + mark;
+    }
 }
--- JDBCProgram/src/HIbernate/StudentApp.java
+++ JDBCProgram/src/HIbernate/StudentApp.java
@@ -1,4 +1,32 @@
 package HIbernate;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+// Hibernate is a framework which is built above JDBC
+// Hibernate is also knows as ORM (Ob)
 public class StudentApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/janbatch?user=root&password=system");
+            String sql = "insert into student values(?,?,?,?,?)";
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            System.out.println("Enter Student ID, name, age, gender, marks");
+            psmt.setInt(1, sc.nextInt());
+            psmt.setString(2, sc.next());
+            psmt.setInt(3, sc.nextInt());
+            psmt.setString(4, sc.next());
+            psmt.setInt(5, sc.nextInt());
+
+            int rowsAffected = psmt.executeUpdate();
+            System.out.println("rows affected: " + rowsAffected);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
