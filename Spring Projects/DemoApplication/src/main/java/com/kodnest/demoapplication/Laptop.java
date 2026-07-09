75713d7da7be19249e9bd8a62353020fc56bb01b
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-07-09 18:49:46 +0530

    Added

--- Spring Projects/DemoApplication/src/main/java/com/kodnest/demoapplication/Laptop.java
+++ Spring Projects/DemoApplication/src/main/java/com/kodnest/demoapplication/Laptop.java
@@ -1,4 +1,15 @@
 package com.kodnest.demoapplication;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop Constructor");
+        System.out.println("Laptop object is created");
+    }
+
+    public void build() {
+        System.out.println("Built the application using Laptop");
+    }
 }
