a77dfda61c93cd346ba77daeccd45bcc60bf4e98
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-07-09 18:47:30 +0530

    Add

--- Spring Projects/2SetterInjection/src/main/java/com/kodnest/_setterinjection/Developer.java
+++ Spring Projects/2SetterInjection/src/main/java/com/kodnest/_setterinjection/Developer.java
@@ -1,4 +1,23 @@
 package com.kodnest._setterinjection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Developer {
+    Laptop laptop;
+
+    public void develop() {
+        laptop.build();
+        System.out.println("Developer completed the work");
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    @Autowired
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
 }
