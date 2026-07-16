77b5caa6e998ee593f505a17609eb9007b3e3f8c
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-07-16 18:56:27 +0530

    Added Percentage Calculator Project

--- Spring Projects/9PercentageCalculator/src/main/java/com/kodnest/_percentagecalculator/MyController.java
+++ Spring Projects/9PercentageCalculator/src/main/java/com/kodnest/_percentagecalculator/MyController.java
@@ -1,7 +1,11 @@
 package com.kodnest._percentagecalculator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
@@ -9,8 +13,19 @@ public class MyController {
     public String displayForm() {
         return "index";
     }
+    @PostMapping("/calculate")
+    public String calculatePercentage(@RequestParam int roll, @RequestParam String name, @RequestParam int m1,
+                                      @RequestParam int m2, @RequestParam int m3, Model model) {
+        double percent = (m1 + m2 + m3) / 3.0;
+        model.addAttribute("rollNo", roll);
+        model.addAttribute("name", name);
+        model.addAttribute("marks1", m1);
+        model.addAttribute("marks2", m2);
+        model.addAttribute("marks3", m3);
+        model.addAttribute("percentage", percent);
 
-    public void calculatePercentage() {
+        return "result";
+
     }
 
 }
