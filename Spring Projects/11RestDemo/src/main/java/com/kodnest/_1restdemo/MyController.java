fd930a98783a52e1750038d436f39a88076916f0
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-07-22 15:20:00 +0530

    Added RESt Api demo project

--- Spring Projects/11RestDemo/src/main/java/com/kodnest/_1restdemo/MyController.java
+++ Spring Projects/11RestDemo/src/main/java/com/kodnest/_1restdemo/MyController.java
@@ -1,4 +1,12 @@
 package com.kodnest._1restdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+    @GetMapping("/greet")
+    public String greeting() {
+        return "Welcome!";
+    }
 }
