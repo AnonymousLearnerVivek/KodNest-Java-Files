bad401790d4551c5919154c66701c85ef4bc6d8c
AnonymousLearnerVivek <viveksurana.021@gmail.com>
2026-07-23 23:05:36 +0530

    Spring Security Demo Added

--- Spring Projects/14SpringSecurityDemo/src/main/java/com/kodnest/_4springsecuritydemo/MyController.java
+++ Spring Projects/14SpringSecurityDemo/src/main/java/com/kodnest/_4springsecuritydemo/MyController.java
@@ -1,4 +1,13 @@
 package com.kodnest._4springsecuritydemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+
+    @GetMapping("/secure")
+    public String loginDemo() {
+        return "Login successful!";
+    }
 }
